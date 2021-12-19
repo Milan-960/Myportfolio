e3f9907e77857567529e50d8387acea8a03986aa
Milan Sachani <79278557+Milan-960@users.noreply.github.com>
2021-12-19 14:22:07 +0100

    Contact form bug fixed 🐞

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -4,14 +4,13 @@ import BaseLayout from "../../layouts/BaseLayout/BaseLayout";
 
 import Form from "./ContactForm/Form";
 import GoogleMap from "./GoogleMap/Map";
-import Information from "./Information/Information";
 
 const Contact = () => {
   return (
     <div>
+      <BaseLayout />
       <Form />
       <GoogleMap />
-      <BaseLayout />
     </div>
   );
 };
