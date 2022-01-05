88682eeb679ff401146a81ed797badaab9ee7bde
Milan Sachani <79278557+Milan-960@users.noreply.github.com>
2022-01-05 19:43:32 +0100

    meta tage and service worker has been changed

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,6 +1,6 @@
 // This optional code is used to register a service worker.
 const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
+  window.location.hostname === "https://milansachani.dev/" ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === "[::1]" ||
     // 127.0.0.0/8 are considered localhost for IPv4.
