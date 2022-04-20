b51aabd1cd96eb3f9e42a2140ffa5e3d9ab606bf
Milan960 <milan.sachani.ms@gmail.com>
2022-04-20 18:17:14 +0200

    certificate has been added

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import BarChart from "../components/BarChart";
 import "./Skills.css";
+import { Button } from "@material-ui/core";
+
+import Pdf2 from "../Document/learning-to-learn-certificate.pdf";
 
 function Skills() {
   return (
@@ -10,6 +13,14 @@ function Skills() {
       style={{ backgroundImage: `url(/skillsbg.jpg)` }}
     >
       <BarChart />
+
+      <div className="certificate">
+        <a rel="noopener noreferrer" href={Pdf2}>
+          <Button variant="contained" className="resume-button">
+            View My Certificate
+          </Button>
+        </a>
+      </div>
     </div>
   );
 }
