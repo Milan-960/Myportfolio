6112876bf8a9f5af0700167dc57139d88effb3fa
Milan960 <milan.sachani.ms@gmail.com>
2022-04-27 18:02:26 +0200

    certificate has been added 🔥

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -5,6 +5,7 @@ import "./Skills.css";
 import { Button } from "@material-ui/core";
 
 import Pdf2 from "../Document/learning-to-learn-certificate.pdf";
+import Pdf3 from "../Document/milan-bitDefree-certificate.pdf";
 
 function Skills() {
   return (
@@ -17,7 +18,12 @@ function Skills() {
       <div className="certificate">
         <a rel="noopener noreferrer" href={Pdf2}>
           <Button variant="contained" className="resume-button">
-            View My Certificate
+            Zero-To-Mastery Certificate
+          </Button>
+        </a>
+        <a rel="noopener noreferrer" href={Pdf3}>
+          <Button variant="contained" className="resume-button">
+            BitDefree Certificate
           </Button>
         </a>
       </div>
