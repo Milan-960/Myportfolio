a116953b1748f8a36b0b7b4100bf006c8dcc2750
Milan960 <milan.sachani.ms@gmail.com>
2022-05-24 18:48:33 +0200

    theme changed

--- src/hooks/themeHook/themeHook.js
+++ src/hooks/themeHook/themeHook.js
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import { ThemeContext } from './themeContext';
+import { useContext } from "react";
+import { ThemeContext } from "./themeContext";
 
 export const useThemeContext = () => {
   return useContext(ThemeContext);
