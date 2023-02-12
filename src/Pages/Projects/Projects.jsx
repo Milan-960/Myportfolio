e1c000d31ad95837e5bd9a05ac5acaa647108562
milan sachani <milan.sachani.ms@gmail.com>
2023-02-12 12:31:51 +0100

    load more project button added

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,9 +1,19 @@
+import React, { useEffect, useState } from "react";
 import s from "./Projects.module.scss";
 import BaseLayout from "../../layouts/BaseLayout/BaseLayout";
 import { PROJECTS } from "../../constants/projects";
 import ProjectCard from "./ProjectCard/ProjectCard";
 
+const imagePerRow = 6;
+
 const Projects = () => {
+  const [next, setNext] = useState(imagePerRow);
+  const handleMoreImage = () => {
+    setNext(next + imagePerRow);
+  };
+  const handleMoreImages = () => {
+    setNext(next - imagePerRow);
+  };
   return (
     <BaseLayout>
       <div className={s.content}>
@@ -13,12 +23,20 @@ const Projects = () => {
         <p className={s.subtitle}>
           Here are a few projects I've worked on recently.
         </p>
-
         <ul className={s.projects}>
-          {PROJECTS.map((props) => (
+          {PROJECTS?.slice(0, next)?.map((props) => (
             <ProjectCard key={props.id} {...props} />
           ))}
         </ul>
+        {next < PROJECTS?.length ? (
+          <button className={s.button} onClick={handleMoreImage}>
+            Load more
+          </button>
+        ) : (
+          <button className={s.button} onClick={handleMoreImages}>
+            Previous
+          </button>
+        )}
       </div>
     </BaseLayout>
   );
