a8be4248433f39886dc9a2492b2fb42498e2e564
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-02-26 15:41:49 +0100

    video added in contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import { useEffect, useState, useRef } from "react";
 
 import BaseLayout from "../../layouts/BaseLayout/BaseLayout";
 
 import Form from "./ContactForm/Form";
 import GoogleMap from "./GoogleMap/Map";
 
+import BgVideo from "../../Video/Fire1.mp4";
+import BgVideo2 from "../../Video/Space.mp4";
+
 const Contact = () => {
+  // This hooks is to play videos
+  const [vidIndex, setVidIndex] = useState(0);
+
+  const ref = useRef(null);
+  useEffect(() => {
+    if (vidIndex === 0 && ref.current) {
+      ref.current.play();
+    }
+  }, [ref, vidIndex]);
+
   return (
     <div>
       <BaseLayout>
-        <Form />
-        <GoogleMap />
+        <div className="main">
+          <video
+            style={{ display: vidIndex === 1 ? "none" : "block" }}
+            src={BgVideo}
+            autoPlay
+            muted
+            onEnded={() => setVidIndex((idx) => idx + 1)}
+            className="bg-vid"
+          />
+          <video
+            style={{ display: vidIndex === 0 ? "none" : "block" }}
+            src={BgVideo2}
+            muted
+            loop
+            ref={ref}
+            className="bg-vid"
+          />
+          <Form />
+          <GoogleMap />
+        </div>
       </BaseLayout>
     </div>
   );
