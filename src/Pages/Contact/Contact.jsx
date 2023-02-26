1d8e798f1320282252b0e6b6918d6e5e9c2cc418
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-02-26 19:41:20 +0100

    fixed video not working on mobile 🔥

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -4,41 +4,14 @@ import BaseLayout from "../../layouts/BaseLayout/BaseLayout";
 
 import Form from "./ContactForm/Form";
 import GoogleMap from "./GoogleMap/Map";
-
-import BgVideo from "../../Video/Fire1.mp4";
-import BgVideo2 from "../../Video/Space.mp4";
+import Video from "./Video";
 
 const Contact = () => {
-  // This hooks is to play videos
-  const [vidIndex, setVidIndex] = useState(0);
-
-  const ref = useRef(null);
-  useEffect(() => {
-    if (vidIndex === 0 && ref.current) {
-      ref.current.play();
-    }
-  }, [ref, vidIndex]);
-
   return (
     <div>
       <BaseLayout>
         <div className="main">
-          <video
-            style={{ display: vidIndex === 1 ? "none" : "block" }}
-            src={BgVideo}
-            autoPlay
-            muted
-            onEnded={() => setVidIndex((idx) => idx + 1)}
-            className="bg-vid"
-          />
-          <video
-            style={{ display: vidIndex === 0 ? "none" : "block" }}
-            src={BgVideo2}
-            muted
-            loop
-            ref={ref}
-            className="bg-vid"
-          />
+          <Video />
           <Form />
           <GoogleMap />
         </div>
