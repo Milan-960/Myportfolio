44e4c146b3e20b539a247fc244094c272fb9feba
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-03-07 11:23:17 +0100

    light mode hotfix

--- src/hooks/themeHook/darkMode-Context.js
+++ src/hooks/themeHook/darkMode-Context.js
@@ -13,13 +13,30 @@ export const DarkModeProvider = ({ children }) => {
   const { isDarkMode } = darkMode;
 
   const toggleDarkModeHandler = () => {
+    // dispatch the action to toggle the dark mode
     dispatch(toggleDarkMode());
+
+    // // save the dark mode state to local storage
+
+    // // set the dark mode class to the body
+    localStorage.setItem("dark", JSON.stringify(!isDarkMode));
+    document.body.classList.toggle("dark");
+    document.body.classList.toggle("light");
   };
 
   useEffect(() => {
-    localStorage.setItem("dark", JSON.stringify(!isDarkMode));
-    document.body.classList.toggle("dark", isDarkMode);
-    document.body.classList.toggle("light", isDarkMode);
+    // get the dark mode state from local storage
+    const darkModeFromStorage = JSON.parse(localStorage.getItem("dark"));
+    // if the dark mode state is true
+    if (darkModeFromStorage) {
+      // set the dark mode class to the body
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      // set the light mode class to the body
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    }
   }, [isDarkMode]);
 
   return (
