9fc6e8aa5de7677eb241b959ae22ef90063b0119
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-03-16 15:54:22 +0100

    fade animation added

--- src/Pages/Contact/GoogleMap/Map.jsx
+++ src/Pages/Contact/GoogleMap/Map.jsx
@@ -2,6 +2,7 @@ import m from "./Map.module.scss";
 
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import Fade from "react-reveal/Fade";
 
 export class MapContainer extends Component {
   constructor(props) {
@@ -30,18 +31,20 @@ export class MapContainer extends Component {
 
   render() {
     return (
-      <div className={m.map}>
-        <Map
-          google={this.props.google}
-          zoom={11}
-          initialCenter={{
-            lat: 52.2297,
-            lng: 21.0122,
-          }}
-        >
-          {this.showMarkers()}
-        </Map>
-      </div>
+      <Fade left>
+        <div className={m.map}>
+          <Map
+            google={this.props.google}
+            zoom={11}
+            initialCenter={{
+              lat: 52.2297,
+              lng: 21.0122,
+            }}
+          >
+            {this.showMarkers()}
+          </Map>
+        </div>
+      </Fade>
     );
   }
 }
