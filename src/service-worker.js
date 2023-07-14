8fa958f5afd6f8f10dfacf98ec4d524db986bb21
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-07-14 12:10:18 +0200

    branch updated 🔥

--- src/service-worker.js
+++ src/service-worker.js
@@ -35,7 +35,7 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
+  createHandlerBoundToURL(process.env.REACT_APP_PUBLIC_URL + "/index.html")
 );
 
 // An example runtime caching route for requests that aren't handled by the
