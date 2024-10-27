1685c9454054cc866489f5cce408a4176d085395
Milan_sacahni <milan.sachani.ms@gmail.com>
2024-10-27 13:57:45 +0100

    📝 (Recommendations.scss): Increase height of carousel-content to 450px for better display 📝 (recommendations.js): Update text content in recommendations to fix typographical errors and improve readability 📝 (recommendations.js): Add a new recommendation with updated text content and information about Milan's contributions and dedication

--- src/constants/recommendations.js
+++ src/constants/recommendations.js
@@ -11,7 +11,7 @@ export const recommendations = [
   {
     img: "https://picsum.photos/id/1025/800/450",
     title: "Exceptional Problem Solver",
-    text: "Working with Milan Sachani across two companies, first at Betfan and later at Spryr, has been a true pleasure. Milan is highly detail-oriented and an exceptional team player, consistently raising the bar for others to grow and evolve. His outstanding programming skills and problem-solving abilities make him a standout, and he is always willing to lend a hand to support his colleagues.\n\nWhat truly sets Milan apart is his strong work ethic and punctuality in all aspects of his work. He has a genuine passion for sharing knowledge in a clear and approachable manner. Milan’s positive influence on the team is undeniable, and I do not doubt he would be a tremendous asset to any organization.\n\nI wholeheartedly recommend Milan.",
+    text: "Working with Milan Sachani across two companies, first at Betfan and later at Spryr, has been a true pleasure. Milan is highly detail-oriented and an exceptional team player, consistently raising the bar for others to grow and evolve. His outstanding programming skills and problem-solving abilities make him a standout, and he is always willing to lend a hand to support his colleagues.\n\nWhat truly sets Milan apart is his strong work ethic and punctuality in all aspects of his work. He has a genuine passion for sharing knowledge in a clear and approachable manner. Milan's positive influence on the team is undeniable, and I do not doubt he would be a tremendous asset to any organization.\n\nI wholeheartedly recommend Milan.",
     name: "Kush Bhalodi",
     position: "Full Stack Engineer",
     recoLinks:
@@ -20,9 +20,18 @@ export const recommendations = [
   {
     img: "https://picsum.photos/id/103/800/450",
     title: "Go-to Guy",
-    text: "I've worked with Milan on the DRIP. Gaming team (now rebranded as Charter Labs), we've brainstormed on ideas and new features to progress through the project at hand, and Milan's insights were always valuable, and his skills show this perfectly, he's the go-to guy when you want a competent & fast engineer.\n\nI'm most definitely sure any team he's with is lucky to have him, because he’ll also bring out the best in them like he did with us, it's been an awesome time working with Milan and hopefully to many other projects",
+    text: "I've worked with Milan on the DRIP. Gaming team (now rebranded as Charter Labs), we've brainstormed on ideas and new features to progress through the project at hand, and Milan's insights were always valuable, and his skills show this perfectly, he's the go-to guy when you want a competent & fast engineer.\n\nI'm most definitely sure any team he's with is lucky to have him, because he'll also bring out the best in them like he did with us, it's been an awesome time working with Milan and hopefully to many other projects",
     name: "Haidar Ezio",
     position: "Senior Frontend Engineer",
+    recoLinks:
+      "https://www.linkedin.com/in/milan-sachani-9090/#:~:text=Recommendations,Recommendations",
+  },
+  {
+    img: "https://picsum.photos/id/196/800/450",
+    title: "Relentless Tech Enthusiast",
+    text: "I've had the privilege of working closely with Milan, and his dedication is truly unparalleled. Whether it's pushing through long hours or finding creative solutions to complex challenges, Milan's commitment to his craft is second to none. His passion for programming and problem-solving drives him to go above and beyond in every project he tackles.\n\n In addition to excelling in his role, Milan is deeply involved in the open-source community. His contributions reflect not only his technical skills but also his desire to give back and improve the tools that developers worldwide rely on. His dedication is evident in the range of projects he works on in his free time, always staying ahead of the curve and pushing the boundaries of what's possible. \n\n Milan's hard work, relentless focus, and willingness to take on any challenge make him an invaluable team member and an inspiration to those around him. I highly recommend Milan to anyone looking for someone who consistently brings passion, talent, and dedication to the table!",
+    name: "Luka Banjac",
+    position: "Full Stack Engineer",
     recoLinks:
       "https://www.linkedin.com/in/milan-sachani-9090/#:~:text=Recommendations,Recommendations",
   },
